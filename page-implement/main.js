b4e6edeb6ea09c0174b6c4b62a195b9d5c8eaece
unimeang <aodgkfud4@gmail.com>
2022-03-10 19:24:13 +0900

    ✅  [add] : main URl 주소 연결

--- page-implement/main.js
+++ page-implement/main.js
@@ -1,6 +1,10 @@
+const url = 'http://35.76.53.28:8080/mall';
+
+
+
 async function getProductData() {
     const res = await fetch(`http://35.76.53.28:8080/mall`), {
         method: "GET",
-        
+
     }
 }
