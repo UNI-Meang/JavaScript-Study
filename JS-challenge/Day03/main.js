25636c241e34c72ce234f864b21a23ccdcaa3949
unimeang <aodgkfud4@gmail.com>
2022-05-17 17:44:54 +0900

    ✅  [feat] : random 숫자 맞추기 기능 구현

---
+++ JS-challenge/Day03/main.js
@@ -0,0 +1,22 @@
+const form = document.getElementById('mainGameForm');
+const rangeNumber = document.querySelector('.rangeNum');
+const inputNumber = document.getElementById('inptNumber');
+
+function playGame(e){
+    e.preventDefault();
+    const range = rangeNumber.value;
+    const inputValue = inputNumber.value;
+    const text = document.querySelector('.hidden');
+    const randomNum = Math.ceil(Math.random() * parseInt(range));
+    text.innerHTML = `
+    <p>You chose: ${inputValue}
+    <p>The machine chose: ${randomNum}
+    `
+    if(randomNum == inputValue){
+        text.innerHTML += `
+        <p class="msgWon">You won!👏</p>`
+    } else {
+        text.innerHTML += `<p class="msgLost"> You lost!💩</p>`
+    }
+}
+form.addEventListener('submit', playGame);
