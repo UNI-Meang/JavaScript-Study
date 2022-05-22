d91cd3500de3f56b221ce0b9c9978a13b94f3b4e
unimeang <aodgkfud4@gmail.com>
2022-05-23 00:50:24 +0900

    ✅  [feat] : login 기능 구현

--- JS-challenge/Momentum_App/app.js
+++ JS-challenge/Momentum_App/app.js
@@ -1,11 +1,31 @@
-const loginForm = document.querySelector('#login-form');
+const loginForm = document.getElementById('login-form');
 const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+
 
 function loginSubmit(e){
     e.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    loginForm.classList.add('hidden');
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener('submit',loginSubmit);
+function paintGreetings(username){
+    greeting.innerHTML = `
+    <p class="greeting"> Welcome! ${username}</p>
+    `;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+const checkUsername = localStorage.getItem(USERNAME_KEY);
+
+if(checkUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener('submit', loginSubmit);
+} else {
+    paintGreetings(username);
+}
