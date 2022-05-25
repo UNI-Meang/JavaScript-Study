6805ce763e77b53b55d03bdf0d7c8fd0048dab69
unimeang <aodgkfud4@gmail.com>
2022-05-25 21:52:55 +0900

    🐛 [mod] : refac

--- JS-challenge/Momentum_App/background.js
+++ JS-challenge/Momentum_App/background.js
@@ -2,17 +2,13 @@ const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-
 const background = document.createElement("img");
 
 background.src = `images/${chosenImage}`;
-
-// const main = document.querySelector('.container');
-
-// main.innerHTML += 'background';
-
-// main.prepend(background);
-
 document.body.prepend(background);
 
+
+// const main = document.querySelector('.container');
+// main.innerHTML += 'background';
+// main.prepend(background);
 // document.body.style.backgroundImage('background');
